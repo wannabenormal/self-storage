0c42a78329a4fe583694895d81c5acb20f816987
CaDiBob <nikolay.v.byankin@gmail.com>
2022-06-15 21:11:01 +0900

    Добавил templates

--- selfstorage/settings.py
+++ selfstorage/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'selfstorage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,6 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
