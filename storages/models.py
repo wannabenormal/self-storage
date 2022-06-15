2f3cdb9661e4dcbd4910e066d1ebdae71798553c
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-06-15 12:41:53 +0300

    Add models for Box and Storage

--- storages/models.py
+++ storages/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Storage(models.model):
+    address = models.CharField('адрес', max_length=100, unique=True)
+    temperature = models.SmallIntegerField('температура')
+    ceiling_height = models.FloatField('Высота в метрах')
+
+
+class Box(models.model):
+    storage = models.ForeignKey(
+        Storage,
+        verbose_name='Склад',
+        related_name='boxes',
+        on_delete=models.CASCADE
+    )
+    number = models.CharField('номер', max_length=20)
+    width = models.FloatField('ширина')
+    length = models.FloatField('длина')
+    height = models.FloatField('высота')
+
+
