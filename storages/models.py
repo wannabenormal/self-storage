4f9653b9cf636cda07e0a4eabe684c89b38a444b
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-06-15 12:50:21 +0300

    Add queryset metods for Box model

--- storages/models.py
+++ storages/models.py
@@ -1,11 +1,20 @@
 from django.db import models
-
+from django.db.models import F
 
 class Storage(models.model):
     address = models.CharField('адрес', max_length=100, unique=True)
     temperature = models.SmallIntegerField('температура')
     ceiling_height = models.FloatField('Высота в метрах')
 
+
+class BoxQuerySet(models.QuerySet):
+    def with_area(self):
+        return self.annotate(
+            area=F('width') * F('length')
+        )
+
+    def empty(self):
+        return self.filter(empty=True)
 
 class Box(models.model):
     storage = models.ForeignKey(
@@ -19,4 +28,5 @@ class Box(models.model):
     length = models.FloatField('длина')
     height = models.FloatField('высота')
 
+    empty = models.BooleanField('свободно')
 
