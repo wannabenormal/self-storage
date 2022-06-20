5fb7fb7787a92e5c08564a0ef3a25e23a5bd6b0b
iterekhov98@gmail.com <iterekhov98@gmail.com>
2022-06-20 16:08:48 +0300

    Create separate app for api functions

--- storages/views.py
+++ storages/views.py
@@ -1,36 +1,11 @@
-import io
-import json
 from datetime import date, timedelta
-from django.http import JsonResponse
-from pytz import timezone
 
-import qrcode
-from django.core.mail import EmailMessage
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
-from django.views.decorators.csrf import csrf_exempt
 
 from .models import Order, Box, Storage
 from geolocation.views import get_nearest_storage
 from users.forms import UserCreationForm
-
-def send_qr_to_renter(data_for_qr:str, renter_email, message):
-    '''
-    Генерирует qr из параметра data_for_qr, отправляет на почту renter_email,
-    с текстом message. При успешной отправке возвращает 1
-    '''
-    code = qrcode.make(data_for_qr)
-    im_resize = code.resize((500, 500))
-    buf = io.BytesIO()
-    im_resize.save(buf, format='png')
-    byte_im = buf.getvalue()
-    email = EmailMessage(
-        subject='Код доступа',
-        body=message,
-        to=(renter_email,)
-    )
-    email.attach('code.png', byte_im)
-    return email.send(fail_silently=False)
 
 
 def is_manager(user):
@@ -50,6 +25,7 @@ def view_orders(request):
         context={'order_items': orders}
     )
 
+@user_passes_test(is_manager, login_url='signin')
 def view_expired_orders(request):
     orders = Order.objects.expired() \
                           .prefetch_related('boxes') \
@@ -95,24 +71,6 @@ def order_details(request, product_number):
     )
 
 
-@csrf_exempt
-def save_order(request):
-    user = request.user
-    order_details = json.loads(request.body.decode('utf-8'))
-    box = Box.objects.select_related('order').get(number=order_details['box'])
-    if box.order and box.order.renter == user:
-        box.order.end_current_rent = order_details['end_rent']
-        box.order.save()
-    else:
-        order = Order.objects.create(
-            renter = user,
-            end_current_rent= date.today() + timedelta(days=30),
-        )
-        order.boxes.add(box)
-        order.save()
-    return JsonResponse({'message': 'ok','redirect': ''})
-
-
 def view_storages(request):
     storages = Storage.objects.with_min_price().with_availability().all()
 
@@ -136,26 +94,4 @@ def view_index(request):
         }
     )
 
-
-def end_rent(user, box_number):
-    box = Box.objects.select_related('order').get(number=box_number)
-    if box.order.renter == user:
-        box.order = None
-        box.save()
-    return
-
-
-@csrf_exempt
-def open_box(request):
-    user = request.user
-    order_details = json.loads(request.body.decode('utf-8'))
-    if order_details.get('end_rent'):
-        end_rent(user, order_details.get('box'))
-    message = f"""Добрый день {user.username}! Код доступа для окрытия вашего бокса.
-              Если вы не запрашивали код, пожалуйста, обратитесь в службу поддержки:   
-              8 (800) 000-00-00 """
-    data_for_qr = 'https://dvmn.org'
-    if send_qr_to_renter(data_for_qr, user.email, message):
-        return JsonResponse({'message': 'ok'})
-    return JsonResponse({'message': 'something went wrong'})
 
